f176bc44da399edeb125c5dfc16324cc1095267b
Aubrey <aubrey.goodman@gmail.com>
2012-11-27 18:39:34 -0500

    Initial Commit

---
+++ Bus Plus Core/Bus_Plus_Core.h
@@ -0,0 +1,13 @@
+//
+//  Bus_Plus_Core.h
+//  Bus Plus Core
+//
+//  Created by Aubrey Goodman on 11/27/12.
+//  Copyright (c) 2012 Migrant Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Bus_Plus_Core : NSObject
+
+@end
---
+++ Bus Plus Core/Bus_Plus_Core.m
@@ -0,0 +1,13 @@
+//
+//  Bus_Plus_Core.m
+//  Bus Plus Core
+//
+//  Created by Aubrey Goodman on 11/27/12.
+//  Copyright (c) 2012 Migrant Studios. All rights reserved.
+//
+
+#import "Bus_Plus_Core.h"
+
+@implementation Bus_Plus_Core
+
+@end
